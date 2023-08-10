c35b5df211eac5af7dab4e7e5134c1dc439dab8d
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-10 00:46:01 -0400

    Added logout script

---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+const logout = async () => {
+    // call to logout in user routes
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    // logging out ends in going to login page
+    if (response.ok) {
+        document.location.replace('/login');
+    } 
+    else {
+        alert(response.statusText);
+    }
+};
+
+// listen for user clicking logout
+document.querySelector('#logout').addEventListener('click', logout);
